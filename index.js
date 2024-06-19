e957c9199f5d20e054a96ded2eb4d03967f4e5e1
samuelnjathi <samuelnjathi85@gmail.com>
2024-06-19 23:16:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+let posts =[];
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        posts: posts
+    });
+});
+
+app.get("/post", (req, res) => {
+    res.render("post.ejs");
+});
+
+app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+});
+
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.title,
+        text: req.body.text,
+        author: req.body.author
+    }
+    posts.push(post);
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`)
+})
